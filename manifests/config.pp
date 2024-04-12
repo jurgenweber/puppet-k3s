a1823d7a34c74ca6427934b0bc59868768d88c8f
Jürgen Weber <jurgen@space-marine.org>
2024-04-12 17:02:26 +1000

    make hte exported resource frfom the init

--- manifests/config.pp
+++ manifests/config.pp
@@ -6,9 +6,27 @@ class k3s::config (
   # type as in the 'init'/first master or a 'joining' master
   case $type {
     'init': {
+      exec { 'init-cluster':
+        command     => "${k3s::binary_path} server --cluster-init",
+        # apparently makes this data dir; https://docs.k3s.io/cli/server#data
+        creates     => '/var/lib/rancher/k3s',
+        environemnt => [
+          "K3S_TOKEN=${token_secret}"
+        ]
+      }
+      # this is the exporter resource, with the ip details, https://www.puppet.com/docs/puppet/7/lang_exported.html
+      @@exec { 'join-cluster':
+        command     => "${k3s::binary_path} server --server ${::ipaddress}:6443",
+        # apparently makes this data dir; https://docs.k3s.io/cli/server#data
+        creates     => '/var/lib/rancher/k3s',
+        environemnt => [
+          "K3S_TOKEN=${token_secret}"
+        ]
+      }
     }
 
     'joining': {
+      Exec <<| |>>
     }
 
     default: {

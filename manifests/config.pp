da356fa398165cd792451c34eb0cc731e6cb0464
Jürgen Weber <jurgen@space-marine.org>
2024-04-12 16:31:21 +1000

    Maybe?

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,7 +1,7 @@
 # @summary Class responsible for configurationk3s
 class k3s::config (
   String $token_secret,
-  String['init','joining'] $type,
+  Enum['init','joining'] $type,
 ) {
   # type as in the 'init'/first master or a 'joining' master
   case $type {

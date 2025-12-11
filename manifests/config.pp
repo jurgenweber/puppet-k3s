9b1139afb387ea16a9057eb9d38022981410962e
Jürgen Weber <jurgen@space-marine.org>
2025-12-11 16:17:31 +1100

    Unique names

--- manifests/config.pp
+++ manifests/config.pp
@@ -93,7 +93,7 @@ class k3s::config () {
         line    => "K3S_TOKEN=${$k3s::token_secret}",
       }
       if ($k3s::runtimeclass != 'default') {
-        file_line { 'add-env-token-to-systemd':
+        file_line { 'add-env-runtime-to-systemd':
           path    => '/etc/systemd/system/k3s-agent.service.env',
           line    => "K3S_DEFAULT_RUNTIME=${$k3s::runtimeclass}",
         }
